28e1d03ae6da30505a67082eaa719b53a1782adb
Emanuele <info@emanueleumberto.it>
2025-06-11 13:43:26 +0200

    Intro teoria SQL

--- TeoriaSQL.sql
+++ TeoriaSQL.sql
@@ -4,6 +4,9 @@
     muti riga
 */
 
+-- https://dbml.dbdiagram.io/docs/
+-- https://dbdiagram.io/home
+
 -- SQL ha diverse categorie di Statement
 /*
 	DDL -> Data Definition Language
@@ -56,6 +59,10 @@
 	ALTER TABLE db_name.table_name DROP CONSTRAINT constraint_name;
 
 	DROP TABLE [IF EXISTS] db_name.table_name [CASCADE | RESTRICT]
+    
+    Relazione -> OneToOne
+    Relazione -> OneToMany
+    Relazione -> ManyToMany
 */
 
 -- DDL -> DATABASE
@@ -64,6 +71,7 @@ CREATE DATABASE IF NOT EXISTS test_db;
 USE test_db;
  
 -- DDL -> TABLE
+-- users -> user_id - firstname - lastname - age - city - fiscal_code
 CREATE TABLE IF NOT EXISTS test_db.users (
 	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
@@ -77,7 +85,91 @@ ALTER TABLE test_db.users RENAME COLUMN name TO firstname;
 -- ALTER TABLE test_db.users CHANGE name firstname VARCHAR(25) NOT NULL;
 ALTER TABLE test_db.users ADD COLUMN fiscal_code VARCHAR(16) NOT NULL UNIQUE;
 ALTER TABLE test_db.users MODIFY fiscal_code CHAR(16);
-ALTER TABLE test_db.users DROP COLUMN city;
+ALTER TABLE test_db.users DROP COLUMN email;
 
-DROP TABLE IF EXISTS test_db.users;
-DROP DATABASE IF EXISTS test_db;
+-- DROP TABLE IF EXISTS test_db.users;
+-- DROP DATABASE IF EXISTS test_db;
+
+-- Relazione OneToOne
+-- signin -> signin_id - email - password - user_id
+CREATE TABLE IF NOT EXISTS test_db.signin (
+	signin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    user_id INT NOT NULL UNIQUE,
+    CONSTRAINT signin_user_fk 
+		FOREIGN KEY(user_id) 
+		REFERENCES test_db.users(user_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+/*CREATE TABLE IF NOT EXISTS test_db.signin (
+	signin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    CONSTRAINT signin_user_fk 
+		FOREIGN KEY(signin_id) 
+		REFERENCES test_db.users(user_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);*/
+
+-- Relazione OneToMany o ManyToOne
+-- cars -> car_id - car_name - car_license_plate - user_id
+CREATE TABLE IF NOT EXISTS test_db.cars (
+	car_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    car_name VARCHAR(50) NOT NULL,
+    car_license_plate CHAR(7) NOT NULL UNIQUE,
+    user_id INT NULL,
+    CONSTRAINT car_user_fk FOREIGN KEY(user_id)
+		REFERENCES test_db.users(user_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Relazione ManyToMany
+-- courses -> course_id - course_name - course_code - course_hours
+CREATE TABLE IF NOT EXISTS test_db.courses (
+	course_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    course_code INT4 NOT NULL,
+    course_hours INT NULL DEFAULT 200
+);
+
+ALTER TABLE test_db.courses ADD CONSTRAINT course_code_uk UNIQUE(course_code);
+
+-- users_courses -> users_courses_id - user_id - course_id - registration_date
+CREATE TABLE IF NOT EXISTS test_db.users_courses (
+	users_courses_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    course_id INT NOT NULL
+    /*
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+						ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT users_courses_fk1 FOREIGN KEY(user_id) 
+		REFERENCES test_db.users(user_id),
+	CONSTRAINT users_courses_fk2 FOREIGN KEY(course_id)
+		REFERENCES test_db.courses(course_id)
+	*/
+);
+
+ALTER TABLE test_db.users_courses 
+		ADD COLUMN registration_date TIMESTAMP 
+        DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+-- ALTER TABLE test_db.users_courses ADD CONSTRAINT users_courses_id_pk PRIMARY KEY(users_courses_id);
+
+ALTER TABLE test_db.users_courses
+		ADD CONSTRAINT users_courses_fk1 FOREIGN KEY(user_id) 
+        REFERENCES test_db.users(user_id)
+        ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE test_db.users_courses
+		ADD CONSTRAINT users_courses_fk2 FOREIGN KEY(course_id)
+        REFERENCES test_db.courses(course_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+
+
+
+
+
+
